1aa53f11891527e59e3887b93d58be905f47c2fc
skilinki <bokoakoko@gmail.com>
2022-12-12 18:57:57 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /** 
 * main - prints a given phrase
 * return : 0 if exited properly, non-zero otherwise 
 */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
   
